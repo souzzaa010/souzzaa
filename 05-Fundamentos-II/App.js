99fa79cf137cc181baa0cd3c9319ae460c513ddd
souzzaa010 <alexandre.p.sousa@iesb.edu.br>
2025-03-27 20:15:49 -0300

    .

--- 05-Fundamentos-II/App.js
+++ 05-Fundamentos-II/App.js
@@ -1,34 +1,56 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Image } from 'react-native';
 import PrimeiroComponente from './Componentes/PrimeiroComponente';
 import JavaScriptComponente from './Componentes/JavaScriptComponente';
 import Perfil from './Componentes/Perfil';
 import ListaCompenentes from './Componentes/ListaComponente';
+import Atleta from './Componentes/Atleta';
 
 export default function App() {
+
+  const listaAtletas = [
+    {
+      nome: "Caça Rato",
+      idade: 38,
+      numero: 7,
+      imagem:'https://i.pinimg.com/736x/51/9a/c2/519ac2b1db61823ba1df9ebdddd68bc4.jpg'
+    },
+    {
+      nome: "Ronaldo",
+      idade: 48,
+      numero: 9,
+      imagem:'https://i.pinimg.com/736x/25/22/2a/25222acc29f054f6b491ceee74747d23.jpg'
+    },
+    {
+      nome: "LeBronha",
+      idade: 38,
+      numero: 23,
+      imagem:'https://i.pinimg.com/736x/7c/48/19/7c481977061c7f68bb5970dae91f1787.jpg'
+    },
+  ]
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
 
-      {/* <Perfil 
-      nome="Alee"
-      idade={22}
-      email="teste@gmail.com"
-      telefone={6199999999}
-      />
-
-      <Perfil 
-      nome="Gayzin"
-      idade={69}
-      email="teste@hotmail.com"
-      telefone={6188888888}
-      /> */}
-      
-      <ListaCompenentes />
-
+        {listaAtletas.map(
+          atleta => {
+            return(
+              <Atleta 
+                nome={atleta.nome}
+                idade={atleta.idade}
+                numero={atleta.numero}
+                imagem={atleta.imagem}
+              />
+            )
+          }
+        )}
 
     </View>
+
+    </ScrollView>
   );
 }
 
