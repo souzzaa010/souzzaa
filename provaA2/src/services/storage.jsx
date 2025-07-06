3b3fda294c318f551b06035d70eed391a980a730
ale <alexandre.p.sousa@iesb.edu.br>
2025-07-06 17:10:30 -0300

    a

--- provaA2/src/services/storage.jsx
+++ provaA2/src/services/storage.jsx
@@ -35,7 +35,7 @@ export async function salvarTarefa(novaTarefa) {
   }
 }
 
-// ✅ Excluir uma tarefa específica por ID
+// ✅ Excluir uma tarefa específica por ID 
 export async function excluirTarefa(id) {
   try {
     const tarefas = await listarTarefas();
